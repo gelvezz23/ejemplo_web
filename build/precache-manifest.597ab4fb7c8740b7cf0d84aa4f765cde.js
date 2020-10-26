ca25bcbc7f9d7591e629436cc8a396129c5633c0
Carlos Gomez <carlosg@titamedia.com>
2020-10-26 06:31:40 -0500

    add project

---
+++ build/precache-manifest.597ab4fb7c8740b7cf0d84aa4f765cde.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391a6fbd4daad02252782e99c5de670d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be536571b188c14d2788",
+    "url": "/static/css/main.bbdf8cb4.chunk.css"
+  },
+  {
+    "revision": "f439f68584fe38a25022",
+    "url": "/static/js/2.8fa4e446.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8fa4e446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be536571b188c14d2788",
+    "url": "/static/js/main.7ba09a6d.chunk.js"
+  },
+  {
+    "revision": "a4c54faf1d2231e197ef",
+    "url": "/static/js/runtime-main.bf3ed384.js"
+  },
+  {
+    "revision": "7311a4d7f75916208d0d9b677aa2c78a",
+    "url": "/static/media/card1.7311a4d7.jpg"
+  },
+  {
+    "revision": "c987b2cdfc00b3978762c9051071aafb",
+    "url": "/static/media/card2.c987b2cd.jpg"
+  },
+  {
+    "revision": "80d2ba19cfcf69d67f0b504c964bb46e",
+    "url": "/static/media/card3.80d2ba19.jpg"
+  },
+  {
+    "revision": "6bf4568366b6da46b1341ddc748b122c",
+    "url": "/static/media/img33.6bf45683.gif"
+  },
+  {
+    "revision": "78cdece69871c65403ae6b5ffa506115",
+    "url": "/static/media/logo_FCE.78cdece6.jpg"
+  },
+  {
+    "revision": "9f574d56f9f5a7a48c85ea2b14bbe4d9",
+    "url": "/static/media/logo_FCE.9f574d56.png"
+  },
+  {
+    "revision": "5cc74ef8a4c422084726eb9dd1163b82",
+    "url": "/static/media/montserrat-v14-latin-regular.5cc74ef8.eot"
+  },
+  {
+    "revision": "6a9e85ac9247f5848db957b873c62e0c",
+    "url": "/static/media/montserrat-v14-latin-regular.6a9e85ac.ttf"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/static/media/montserrat-v14-latin-regular.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/static/media/montserrat-v14-latin-regular.bc3aa95d.woff2"
+  },
+  {
+    "revision": "f3fef7e587e6c436df1d7985b2a90ada",
+    "url": "/static/media/montserrat-v14-latin-regular.f3fef7e5.svg"
+  }
+]);
